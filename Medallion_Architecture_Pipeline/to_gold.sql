2fe8b91d9a529733dc87c164cbef5ee10c3f3c48
Matheus Monteiro Alves Melo <matheus02monteiro02@gmail.com>
2026-03-04 14:52:17 -0300

    Create materialized view broadcast_gold

---
+++ Medallion_Architecture_Pipeline/to_gold.sql
@@ -0,0 +1,14 @@
+CREATE OR REFRESH MATERIALIZED VIEW broadcast_gold AS
+WITH log_identifier_1 AS (
+  SELECT rli.logServiceID, rli.LogIdentifierID
+    FROM referencetables.log_identifier rli
+  WHERE rli.PrimaryFG = 1
+)
+ SELECT dbs.*, rcpc.EnglishDescription, li.LogIdentifierID, dbkg.prediction
+    FROM default.broadcast_silver dbs
+      LEFT JOIN referencetables.cd_program_class rcpc
+        ON dbs.ProgramClassID = rcpc.ProgramClassID
+      LEFT JOIN log_identifier_1 li
+        ON dbs.LogServiceID = li.LogServiceID
+      LEFT JOIN default.broadcast_kmeans_gold dbkg
+        ON dbs.BroadcastLogID = dbkg.broadcastLogID
