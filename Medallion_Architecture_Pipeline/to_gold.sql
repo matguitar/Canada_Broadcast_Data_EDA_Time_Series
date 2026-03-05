65da9eb23b0b3a3096b7af48377fb1e524cce111
Matheus Monteiro Alves Melo <matheus02monteiro02@gmail.com>
2026-03-05 09:35:03 -0300

    Refactor broadcast_gold view to select specific columns
    Updated the materialized view to include specific columns from the broadcast_silver table and join with additional reference tables.

--- Medallion_Architecture_Pipeline/to_gold.sql
+++ Medallion_Architecture_Pipeline/to_gold.sql
@@ -1,10 +1,28 @@
+-- Query para a camada gold
+
 CREATE OR REFRESH MATERIALIZED VIEW broadcast_gold AS
 WITH log_identifier_1 AS (
   SELECT rli.logServiceID, rli.LogIdentifierID
     FROM referencetables.log_identifier rli
   WHERE rli.PrimaryFG = 1
 )
- SELECT dbs.*, rcpc.EnglishDescription, li.LogIdentifierID, dbkg.prediction
+ SELECT 
+  dbs.BroadcastLogID,
+  dbs.LogServiceID, 
+  dbs.ClosedCaptionID,
+  dbs.ProgramClassID,
+  dbs.ProgramTitle,
+  dbs.LogEntryDate,
+  dbs.year,
+  dbs.month,
+  dbs.day,
+  dbs.Quarter,
+  dbs.Duration_seconds,
+  dbs.EndTime,
+  dbs.StartTime,
+  rcpc.EnglishDescription, 
+  li.LogIdentifierID, 
+  dbkg.prediction
     FROM default.broadcast_silver dbs
       LEFT JOIN referencetables.cd_program_class rcpc
         ON dbs.ProgramClassID = rcpc.ProgramClassID
